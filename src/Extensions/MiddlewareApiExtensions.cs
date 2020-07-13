829dd8ea40e2ed23cdcbb18e4693124efdb9fc2d
Massimo Giambona <massimo.giambona@gmail.com>
2020-07-13 13:23:24 +0200

    Fixed nuspec file Added comments

--- src/Extensions/MiddlewareApiExtensions.cs
+++ src/Extensions/MiddlewareApiExtensions.cs
@@ -5,14 +5,31 @@ namespace Solidcube.AspNetCore.ProxyHelper.Extensions
 {
     public static class MiddlewareApiExtensions
     {
+        /// <summary>
+        /// The default heder key name.
+        /// </summary>
         public static readonly string XForwardedHeaderPath = "X-Forwarded-Path";
 
+        /// <summary>
+        /// Uses the forwarded header path.
+        /// Read the path from the header key "X-Forwarded-Path".
+        /// </summary>
+        /// <param name="app">The application.</param>
+        /// <returns>Tha application.</returns>
         public static IApplicationBuilder UseForwardedHeaderPath(this IApplicationBuilder app)
         {
             ProxyPathOptions o = new ProxyPathOptions { XForwardedHeaderPath = XForwardedHeaderPath };
             return app.UseMiddleware<ProxyPathMiddleware>(o);
         }
 
+        /// <summary>
+        /// Uses the forwarded header path. 
+        /// Read the path from the hader key specified by the options.
+        /// </summary>
+        /// <param name="app">The application.</param>
+        /// <param name="options">The options.</param>
+        /// <returns>The application.</returns>
+        /// <exception cref="System.ArgumentNullException">options</exception>
         public static IApplicationBuilder UseForwardedHeaderPath(this IApplicationBuilder app, ProxyPathOptions options)
         {
             if (options is null)
--- src/Models/ProxyPathOptions.cs
+++ src/Models/ProxyPathOptions.cs
@@ -1,7 +1,16 @@
 ﻿namespace Solidcube.AspNetCore.ProxyHelper.Models
 {
+    /// <summary>
+    /// The forwarded header path options
+    /// </summary>
     public class ProxyPathOptions
     {
+        /// <summary>
+        /// Gets or sets the header key contains the path.
+        /// </summary>
+        /// <value>
+        /// The header key.
+        /// </value>
         public string XForwardedHeaderPath { get; set; }
     }
 }
